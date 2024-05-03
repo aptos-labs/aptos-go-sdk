25ed559ac2cb3df8d238975c5522b094efda94ed
Greg Nazario <greg@gnazar.io>
2024-05-03 15:09:35 -0700

    [examples] Add some simple examples to build with the SDK

---
+++ examples/performance_transaction/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"github.com/aptos-labs/aptos-go-sdk"
+	"github.com/aptos-labs/aptos-go-sdk/examples"
+	"time"
+)
+
+// main This example shows you how to improve performance of the transaction submission
+//
+// Speed can be improved by locally handling the sequence number, gas price, and other factors
+func main() {
+	start := time.Now()
+	before := time.Now()
+	// Create a client for Aptos
+	client, err := aptos.NewClient(aptos.DevnetConfig)
+	if err != nil {
+		panic("Failed to create client:" + err.Error())
+	}
+	println("New client:    ", time.Since(before).Milliseconds(), "ms")
+
+	// Create a sender locally
+	sender, err := aptos.NewEd25519Account()
+	if err != nil {
+		panic("Failed to create sender:" + err.Error())
+	}
+
+	println("Create sender:", time.Since(before).Milliseconds(), "ms")
+
+	before = time.Now()
+
+	// Fund the sender with the faucet to create it onchain
+	err = client.Fund(sender.Address, 100_000_000)
+
+	println("Fund sender:", time.Since(before).Milliseconds(), "ms")
+
+	before = time.Now()
+
+	// Prep arguments
+	receiver := aptos.AccountAddress{}
+	err = receiver.ParseStringRelaxed("0xBEEF")
+	if err != nil {
+		panic("Failed to parse address:" + err.Error())
+	}
+	amount := uint64(100)
+
+	// Serialize arguments
+	// TODO: this seems verbose
+	serializer := aptos.Serializer{}
+	serializer.Struct(&receiver)
+	err = serializer.Error()
+	if err != nil {
+		panic("Failed to serialize arguments:" + err.Error())
+	}
+	receiverArg := serializer.ToBytes()
+
+	serializer = aptos.Serializer{}
+	serializer.U64(amount)
+	err = serializer.Error()
+	if err != nil {
+		panic("Failed to serialize arguments:" + err.Error())
+	}
+	amountArg := serializer.ToBytes()
+
+	rawTxn, err := client.BuildTransaction(sender.Address,
+		aptos.TransactionPayload{Payload: &aptos.EntryFunction{
+			Module: aptos.ModuleId{
+				Address: aptos.AccountOne,
+				Name:    "aptos_account",
+			},
+			Function: "transfer",
+			ArgTypes: []aptos.TypeTag{},
+			Args:     [][]byte{receiverArg, amountArg},
+		}}, aptos.SequenceNumber(0)) // Use the sequence number to skip fetching it
+	if err != nil {
+		panic("Failed to build transaction:" + err.Error())
+	}
+
+	println("Build transaction:", time.Since(before).Milliseconds(), "ms")
+
+	// Sign transaction
+	before = time.Now()
+
+	// TODO: maybe we should flip rawTxn.Sign to PrivateKey.Sign(rawTxn)
+	signedTxn, err := rawTxn.Sign(sender)
+	if err != nil {
+		panic("Failed to sign transaction:" + err.Error())
+	}
+	println("Sign transaction:", time.Since(before).Milliseconds(), "ms")
+
+	before = time.Now()
+	submitResult, err := client.SubmitTransaction(signedTxn)
+	if err != nil {
+		panic("Failed to submit transaction:" + err.Error())
+	}
+	txnHash := submitResult["hash"].(string)
+	println("Submit transaction:", time.Since(before).Milliseconds(), "ms")
+
+	// Wait for the transaction
+	before = time.Now()
+	txn, err := client.WaitForTransaction(txnHash)
+	if err != nil {
+		panic("Failed to wait for transaction:" + err.Error())
+	}
+	println("Wait for transaction:", time.Since(before).Milliseconds(), "ms")
+
+	println("Total time:    ", time.Since(start).Milliseconds(), "ms")
+	println(examples.PrettyJson(txn))
+}
