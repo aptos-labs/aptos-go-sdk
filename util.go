bab6ad5e78d86d2e8af95fc69d37a7ced3e1c220
Greg Nazario <greg@gnazar.io>
2024-05-15 20:05:31 -0700

    [util] Add testing coverage to util, allow it to be used by devs

--- util.go
+++ util.go
@@ -4,8 +4,15 @@ import (
 	"github.com/aptos-labs/aptos-go-sdk/internal/util"
 )
 
+// -- Note these are copied from internal/util/util.go to prevent package loops, but still allow devs to use it
+
 // ParseHex Convenience function to deal with 0x at the beginning of hex strings
 func ParseHex(hexStr string) ([]byte, error) {
 	// This had to be redefined separately to get around a package loop
 	return util.ParseHex(hexStr)
+}
+
+// SHA3_256Hash takes a hash of the given sets of bytes
+func SHA3_256Hash(bytes [][]byte) (output []byte) {
+	return util.SHA3_256Hash(bytes)
 }
