cd1e4276bfacdc76aa90086f0c5b60010643ccac
Greg Nazario <greg@gnazar.io>
2024-06-06 20:04:56 +0700

    [authkey] Add test coverage to 100%

---
+++ crypto/authenticationKey_test.go
@@ -0,0 +1,38 @@
+package crypto
+
+import (
+	"github.com/aptos-labs/aptos-go-sdk/internal/util"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const testAuthKey = "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+
+func TestAuthenticationKey_CryptoMaterial(t *testing.T) {
+	authKeyBytes, err := util.ParseHex(testAuthKey)
+	assert.NoError(t, err)
+
+	authKeyFromString := &AuthenticationKey{}
+	err = authKeyFromString.FromHex(testAuthKey)
+	assert.NoError(t, err)
+
+	authKeyFromBytes := &AuthenticationKey{}
+	err = authKeyFromBytes.FromBytes(authKeyBytes)
+	assert.NoError(t, err)
+
+	assert.Equal(t, authKeyFromString, authKeyFromBytes)
+
+	derivedBytes := authKeyFromString.Bytes()
+	assert.Equal(t, authKeyBytes, derivedBytes)
+	hexString := authKeyFromString.ToHex()
+	assert.Equal(t, testAuthKey, hexString)
+}
+
+func TestAuthenticationKey_CryptoMaterialError(t *testing.T) {
+	authKey := &AuthenticationKey{}
+	err := authKey.FromHex("0x123456")
+	assert.Error(t, err) // Not long enough
+
+	err = authKey.FromHex("abcde")
+	assert.Error(t, err) // Not a string
+}
