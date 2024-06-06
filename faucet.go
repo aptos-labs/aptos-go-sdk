c58843e15ff71cb208048baf86f8dd69fcd141a6
Greg Nazario <greg@gnazar.io>
2024-06-06 20:39:21 +0700

    [faucet] Add documentation

--- faucet.go
+++ faucet.go
@@ -9,11 +9,14 @@ import (
 	"strconv"
 )
 
+// FaucetClient uses the underlying NodeClient to request for APT for gas on a network.
+// This can only be used in a test network (e.g. Localnet, Devnet, Testnet)
 type FaucetClient struct {
 	nodeClient *NodeClient
 	url        *url.URL
 }
 
+// NewFaucetClient creates a new client specifically for requesting faucet funds
 func NewFaucetClient(nodeClient *NodeClient, faucetUrl string) (*FaucetClient, error) {
 	parsedUrl, err := url.Parse(faucetUrl)
 	if err != nil {
@@ -30,11 +33,15 @@ func (faucetClient *FaucetClient) Fund(address AccountAddress, amount uint64) er
 	if faucetClient.nodeClient == nil {
 		return errors.New("faucet's node-client not initialized")
 	}
+
+	// Build URL
 	mintUrl := faucetClient.url.JoinPath("mint")
 	params := url.Values{}
 	params.Set("amount", strconv.FormatUint(amount, 10))
 	params.Set("address", address.String())
 	mintUrl.RawQuery = params.Encode()
+
+	// Make request fo rfunds
 	response, err := faucetClient.nodeClient.Post(mintUrl.String(), "text/plain", nil)
 	if err != nil {
 		return err
@@ -53,6 +60,8 @@ func (faucetClient *FaucetClient) Fund(address AccountAddress, amount uint64) er
 		// no Aptos client to wait on txn completion
 		return nil
 	}
+
+	// Wait for fund transactions to go through
 	slog.Debug("FundAccount wait for transactions", "number of txns", len(txnHashes))
 	if len(txnHashes) == 1 {
 		_, err = faucetClient.nodeClient.WaitForTransaction(txnHashes[0])
