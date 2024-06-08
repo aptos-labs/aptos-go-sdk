0b61e74981de4993c2d09547f409a5ed27ac96a4
Greg Nazario <greg@gnazar.io>
2024-06-08 18:36:09 +0900

    [signedTransaction] Document SignedTransaction

--- signedTransaction.go
+++ signedTransaction.go
@@ -6,6 +6,8 @@ import (
 	"github.com/aptos-labs/aptos-go-sdk/crypto"
 )
 
+type SignedTransactionVariant uint8
+
 // TransactionSigner is a generic interface for a way to sign transactions.  The default implementation is Account
 //
 // Note that AccountAddress is needed to be the correct on-chain value for proper signing.  This may differ from the
@@ -17,19 +19,14 @@ type TransactionSigner interface {
 	AccountAddress() AccountAddress
 }
 
+//region SignedTransaction
+
+const UserTransactionVariant SignedTransactionVariant = 0
+
 // SignedTransaction a raw transaction plus its authenticator for a fully verifiable message
 type SignedTransaction struct {
 	Transaction   RawTransactionImpl
 	Authenticator *TransactionAuthenticator
-}
-
-func (txn *SignedTransaction) MarshalBCS(bcs *bcs.Serializer) {
-	txn.Transaction.MarshalBCS(bcs)
-	txn.Authenticator.MarshalBCS(bcs)
-}
-func (txn *SignedTransaction) UnmarshalBCS(bcs *bcs.Deserializer) {
-	txn.Transaction.UnmarshalBCS(bcs)
-	txn.Authenticator.UnmarshalBCS(bcs)
 }
 
 // Verify checks a signed transaction's signature
@@ -44,6 +41,12 @@ func (txn *SignedTransaction) Verify() error {
 	return errors.New("signature is invalid")
 }
 
+// TransactionPrefix is a cached hash prefix for taking transaction hashes
+var TransactionPrefix *[]byte
+
+// Hash takes the hash of the SignedTransaction
+//
+// Note: At the moment, this assumes that the transaction is a UserTransaction
 func (txn *SignedTransaction) Hash() ([]byte, error) {
 	if TransactionPrefix == nil {
 		hash := Sha3256Hash([][]byte{[]byte("APTOS::Transaction")})
@@ -58,7 +61,19 @@ func (txn *SignedTransaction) Hash() ([]byte, error) {
 	// Transaction signature is defined as, the domain separated prefix based on struct (Transaction)
 	// Then followed by the type of the transaction for the enum, UserTransaction is 0
 	// Then followed by BCS encoded bytes of the signed transaction
-	return Sha3256Hash([][]byte{*TransactionPrefix, {0}, txnBytes}), nil
+	return Sha3256Hash([][]byte{*TransactionPrefix, {byte(UserTransactionVariant)}, txnBytes}), nil
 }
 
-var TransactionPrefix *[]byte
+//region SignedTransaction bcs.Struct
+
+func (txn *SignedTransaction) MarshalBCS(bcs *bcs.Serializer) {
+	txn.Transaction.MarshalBCS(bcs)
+	txn.Authenticator.MarshalBCS(bcs)
+}
+func (txn *SignedTransaction) UnmarshalBCS(bcs *bcs.Deserializer) {
+	txn.Transaction.UnmarshalBCS(bcs)
+	txn.Authenticator.UnmarshalBCS(bcs)
+}
+
+//endregion
+//endregion
