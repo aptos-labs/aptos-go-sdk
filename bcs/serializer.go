f72a3cad12e87f5ede5e7dc47d0df9338b4d75e0
Greg Nazario <greg@gnazar.io>
2024-06-10 14:58:49 -0700

    [bcs] Add more documentation

--- bcs/serializer.go
+++ bcs/serializer.go
@@ -9,12 +9,27 @@ import (
 )
 
 // Serializer is a holding type to serialize a set of items into one shared buffer
+//
+//	serializer := &Serializer{}
+//	serializer.U64(uint64(10))
+//	serializedBytes := serializer.ToBytes()
 type Serializer struct {
-	out bytes.Buffer
-	err error
+	out bytes.Buffer // current serialized bytes
+	err error        // any error that has occurred during serialization
 }
 
 // Serialize serializes a single item
+//
+//	type MyStruct struct {
+//	  num uint64
+//	}
+//
+//	func (str *MyStruct) MarshalBCS(ser *Serialize) {
+//		ser.U64(num)
+//	}
+//
+//	struct := &MyStruct{ num: 100 }
+//	bytes, _ := Serialize(struct)
 func Serialize(value Marshaler) (bytes []byte, err error) {
 	return SerializeSingle(func(ser *Serializer) {
 		value.MarshalBCS(ser)
@@ -26,12 +41,12 @@ func (ser *Serializer) Error() error {
 	return ser.err
 }
 
-// SetError If the data is well-formed but nonsense, MarshalBCS() code can set error
+// SetError If the data is well-formed but nonsense, [Marshaler.MarshalBCS] code can set the error.
 func (ser *Serializer) SetError(err error) {
 	ser.err = err
 }
 
-// Bool serialize a bool into a single byte
+// Bool serialize a bool into a single byte, 0x01 for true and 0x00 for false
 func (ser *Serializer) Bool(v bool) {
 	if v {
 		ser.U8(1)
@@ -45,44 +60,46 @@ func (ser *Serializer) U8(v uint8) {
 	ser.out.WriteByte(v)
 }
 
-// U16 serialize an unsigned 16 bit integer
+// U16 serialize an unsigned 16-bit integer in little-endian format
 func (ser *Serializer) U16(v uint16) {
 	var ub [2]byte
 	binary.LittleEndian.PutUint16(ub[:], v)
 	ser.out.Write(ub[:])
 }
 
-// U32 serialize an unsigned 32 bit integer
+// U32 serialize an unsigned 32-bit integer in little-endian format
 func (ser *Serializer) U32(v uint32) {
 	var ub [4]byte
 	binary.LittleEndian.PutUint32(ub[:], v)
 	ser.out.Write(ub[:])
 }
 
-// U64 serialize an unsigned 64 bit integer
+// U64 serialize an unsigned 64-bit integer in little-endian format
 func (ser *Serializer) U64(v uint64) {
 	var ub [8]byte
 	binary.LittleEndian.PutUint64(ub[:], v)
 	ser.out.Write(ub[:])
 }
 
-// U128 serialize an unsigned 128 bit integer
+// U128 serialize an unsigned 128-bit integer in little-endian format
 func (ser *Serializer) U128(v big.Int) {
 	var ub [16]byte
 	v.FillBytes(ub[:])
+	// Reverse, since big.Int outputs bytes in BigEndian
 	slices.Reverse(ub[:])
 	ser.out.Write(ub[:])
 }
 
-// U256 serialize an unsigned 256 bit integer
+// U256 serialize an unsigned 256-bit integer in little-endian format
 func (ser *Serializer) U256(v big.Int) {
 	var ub [32]byte
 	v.FillBytes(ub[:])
+	// Reverse, since big.Int outputs bytes in BigEndian
 	slices.Reverse(ub[:])
 	ser.out.Write(ub[:])
 }
 
-// Uleb128 serialize an unsigned 32-bit integer as an Uleb128.  This is used specifically for sequence lengths
+// Uleb128 serialize an unsigned 32-bit integer as an Uleb128.  This is used specifically for sequence lengths, and enums.
 func (ser *Serializer) Uleb128(v uint32) {
 	for v > 0x80 {
 		nb := uint8(v & 0x7f)
@@ -92,24 +109,24 @@ func (ser *Serializer) Uleb128(v uint32) {
 	ser.out.WriteByte(uint8(v & 0x7f))
 }
 
-// WriteBytes serialize an array of bytes with its length first as an uleb128
+// WriteBytes serialize an array of bytes with its length first as an Uleb128.
 func (ser *Serializer) WriteBytes(v []byte) {
 	ser.Uleb128(uint32(len(v)))
 	ser.out.Write(v)
 }
 
-// WriteString similar to WriteBytes using the UTF-8 byte representation of the string
+// WriteString similar to [Serializer.WriteBytes] using the UTF-8 byte representation of the string
 func (ser *Serializer) WriteString(v string) {
 	ser.WriteBytes([]byte(v))
 }
 
-// FixedBytes similar to WriteBytes, but it forgoes the length header.  This is useful if you know the fixed length
-// size of the data, such as AccountAddress
+// FixedBytes similar to [Serializer.WriteBytes], but it forgoes the length header.
+// This is useful if you know the fixed length size of the data, such as AccountAddress
 func (ser *Serializer) FixedBytes(v []byte) {
 	ser.out.Write(v)
 }
 
-// Struct uses custom serialization for a Marshaler implementation
+// Struct uses custom serialization for a [Marshaler] implementation.
 func (ser *Serializer) Struct(v Marshaler) {
 	v.MarshalBCS(ser)
 }
@@ -125,7 +142,27 @@ func (ser *Serializer) Reset() {
 	ser.err = nil
 }
 
-// SerializeSequence serializes a sequence of Marshaler implemented types.  Prefixed with the length of the sequence
+// SerializeSequence serializes a sequence of [Marshaler] implemented types.  Prefixed with the length of the sequence.
+//
+// It works with both array values by reference and by value:
+//
+//	type MyStruct struct {
+//		num uint64
+//	}
+//
+//	func (str *MyStruct) MarshalBCS(ser *Serialize) {
+//		ser.U64(num)
+//	}
+//
+//	myArray := []MyStruct{
+//		MyStruct{num: 0},
+//		MyStruct{num: 1},
+//		MyStruct{num: 2},
+//	}
+//
+//	serializer := &Serializer{}
+//	SerializeSequence(myArray, ser)
+//	bytes := serializer.ToBytes()
 func SerializeSequence[AT []T, T any](array AT, ser *Serializer) {
 	SerializeSequenceWithFunction(array, ser, func(ser *Serializer, item T) {
 		// Check if by value is Marshaler
@@ -147,6 +184,12 @@ func SerializeSequence[AT []T, T any](array AT, ser *Serializer) {
 }
 
 // SerializeSequenceWithFunction allows custom serialization of a sequence, which can be useful for non-bcs.Struct types
+//
+//	array := []string{"hello", "blockchain"}
+//
+//	SerializeSequenceWithFunction(array, func(ser *Serializer, item string) {
+//		ser.WriteString(item)
+//	}
 func SerializeSequenceWithFunction[AT []T, T any](array AT, ser *Serializer, serialize func(ser *Serializer, item T)) {
 	ser.Uleb128(uint32(len(array)))
 	for i, v := range array {
@@ -159,50 +202,98 @@ func SerializeSequenceWithFunction[AT []T, T any](array AT, ser *Serializer, ser
 	}
 }
 
+// SerializeSequenceOnly serializes a sequence into a single value using [SerializeSequence]
+//
+//	type MyStruct struct {
+//		num uint64
+//	}
+//
+//	func (str *MyStruct) MarshalBCS(ser *Serialize) {
+//		ser.U64(num)
+//	}
+//
+//	myArray := []MyStruct{
+//		MyStruct{num: 0},
+//		MyStruct{num: 1},
+//		MyStruct{num: 2},
+//	}
+//
+//	bytes, err := SerializeSequenceOnly(myArray)
 func SerializeSequenceOnly[AT []T, T any](input AT) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		SerializeSequence(input, ser)
 	})
 }
 
+// SerializeBool Serializes a single boolean
+//
+//	bytes, _ := SerializeBool(true)
 func SerializeBool(input bool) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.Bool(input)
 	})
 }
 
+// SerializeU8 Serializes a single uint8
+//
+//	bytes, _ := SerializeU8(uint8(200))
 func SerializeU8(input uint8) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.U8(input)
 	})
 }
 
+// SerializeU16 Serializes a single uint16
+//
+//	bytes, _ := SerializeU16(uint16(50000))
 func SerializeU16(input uint16) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.U16(input)
 	})
 }
+
+// SerializeU32 Serializes a single uint32
+//
+//	bytes, _ := SerializeU32(uint32(50000))
 func SerializeU32(input uint32) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.U32(input)
 	})
 }
+
+// SerializeU64 Serializes a single uint64
+//
+//	bytes, _ := SerializeU64(uint64(20))
 func SerializeU64(input uint64) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.U64(input)
 	})
 }
+
+// SerializeU128 Serializes a single uint128
+//
+//	u128 := big.NewInt(1)
+//	bytes, _ := SerializeU128(u128)
 func SerializeU128(input big.Int) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.U128(input)
 	})
 }
+
+// SerializeU256 Serializes a single uint256
+//
+//	u256 := big.NewInt(1)
+//	bytes, _ := SerializeU256(u256)
 func SerializeU256(input big.Int) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.U256(input)
 	})
 }
 
+// SerializeBytes Serializes a single byte array
+//
+//	input := []byte{0x1, 0x2}
+//	bytes, _ := SerializeBytes(input)
 func SerializeBytes(input []byte) ([]byte, error) {
 	return SerializeSingle(func(ser *Serializer) {
 		ser.WriteBytes(input)
@@ -210,6 +301,16 @@ func SerializeBytes(input []byte) ([]byte, error) {
 }
 
 // SerializeSingle is a convenience function, to not have to create a serializer to serialize one value
+//
+// Here's an example for handling a nested byte array
+//
+//	input := [][]byte{[]byte{0x1}, []byte{0x2}}
+//	bytes, _ := SerializeSingle(func(ser *Serializer) {
+//		ser.Uleb128(len(input))
+//		for _, list := range input {
+//			ser.WriteBytes(list)
+//		}
+//	})
 func SerializeSingle(marshal func(ser *Serializer)) (bytes []byte, err error) {
 	ser := &Serializer{}
 	marshal(ser)
