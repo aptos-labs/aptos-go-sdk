bc2e53a23f10eb561c5a450f3d285ae0a065bc7f
Greg Nazario <greg@gnazar.io>
2024-06-26 15:13:25 -0700

    [crypto] Add more docs

--- crypto/authenticator.go
+++ crypto/authenticator.go
@@ -5,19 +5,25 @@ import (
 	"github.com/aptos-labs/aptos-go-sdk/bcs"
 )
 
-// AccountAuthenticatorImpl an implementation of an authenticator to provide generic verification across multiple types
+// AccountAuthenticatorImpl an implementation of an authenticator to provide generic verification across multiple types.
+//
+// Types:
+//   - [Ed25519Authenticator]
+//   - [MultiEd25519Authenticator]
+//   - [SingleKeyAuthenticator]
+//   - [MultiKeyAuthenticator]
 type AccountAuthenticatorImpl interface {
 	bcs.Struct
 
 	// PublicKey is the public key that can be used to verify the signature.  It must be a valid on-chain representation
-	// and cannot be something like Secp256k1PublicKey on its own.
+	// and cannot be something like [Secp256k1PublicKey] on its own.
 	PublicKey() PublicKey
 
 	// Signature is a typed signature that can be verified by the public key. It must be a valid on-chain representation
-	//	// and cannot be something like Secp256k1Signature on its own.
+	// and cannot be something like [Secp256k1Signature] on its own.
 	Signature() Signature
 
-	// Verify Return true if the AccountAuthenticator can be cryptographically verified
+	// Verify Return true if the [AccountAuthenticator] can be cryptographically verified
 	Verify(data []byte) bool
 }
 
@@ -27,29 +33,37 @@ type AccountAuthenticatorImpl interface {
 type AccountAuthenticatorType uint8
 
 const (
-	AccountAuthenticatorEd25519      AccountAuthenticatorType = 0
-	AccountAuthenticatorMultiEd25519 AccountAuthenticatorType = 1
-	AccountAuthenticatorSingleSender AccountAuthenticatorType = 2
-	AccountAuthenticatorMultiKey     AccountAuthenticatorType = 3
+	AccountAuthenticatorEd25519      AccountAuthenticatorType = 0 // AccountAuthenticatorEd25519 is the authenticator type for ed25519 accounts
+	AccountAuthenticatorMultiEd25519 AccountAuthenticatorType = 1 // AccountAuthenticatorMultiEd25519 is the authenticator type for multi-ed25519 accounts
+	AccountAuthenticatorSingleSender AccountAuthenticatorType = 2 // AccountAuthenticatorSingleSender is the authenticator type for single-key accounts
+	AccountAuthenticatorMultiKey     AccountAuthenticatorType = 3 // AccountAuthenticatorMultiKey is the authenticator type for multi-key accounts
 )
 
 // AccountAuthenticator a generic authenticator type for a transaction
-// Implements [AccountAuthenticatorImpl], [bcs.Marshaler], [bcs.Unmarshaler], [bcs.Struct]
+//
+// Implements:
+//   - [AccountAuthenticatorImpl]
+//   - [bcs.Marshaler]
+//   - [bcs.Unmarshaler]
+//   - [bcs.Struct]
 type AccountAuthenticator struct {
-	Variant AccountAuthenticatorType
-	Auth    AccountAuthenticatorImpl
+	Variant AccountAuthenticatorType // Variant is the type of authenticator
+	Auth    AccountAuthenticatorImpl // Auth is the actual authenticator
 }
 
 //region AccountAuthenticator AccountAuthenticatorImpl implementation
 
+// PubKey returns the public key of the authenticator
 func (ea *AccountAuthenticator) PubKey() PublicKey {
 	return ea.Auth.PublicKey()
 }
 
+// Signature returns the signature of the authenticator
 func (ea *AccountAuthenticator) Signature() Signature {
 	return ea.Auth.Signature()
 }
 
+// Verify returns true if the authenticator can be cryptographically verified
 func (ea *AccountAuthenticator) Verify(data []byte) bool {
 	return ea.Auth.Verify(data)
 }
@@ -58,11 +72,19 @@ func (ea *AccountAuthenticator) Verify(data []byte) bool {
 
 //region AccountAuthenticator bcs.Struct implementation
 
+// MarshalBCS serializes the [AccountAuthenticator] to the BCS format
+//
+// Implements:
+//   - [bcs.Marshaler]
 func (ea *AccountAuthenticator) MarshalBCS(ser *bcs.Serializer) {
 	ser.Uleb128(uint32(ea.Variant))
 	ea.Auth.MarshalBCS(ser)
 }
 
+// UnmarshalBCS deserializes the [AccountAuthenticator] from the BCS format
+//
+// Implements:
+//   - [bcs.Unmarshaler]
 func (ea *AccountAuthenticator) UnmarshalBCS(des *bcs.Deserializer) {
 	kindNum := des.Uleb128()
 	if des.Error() != nil {
--- crypto/secp256k1.go
+++ crypto/secp256k1.go
@@ -10,20 +10,28 @@ import (
 
 //region Secp256k1PrivateKey
 
+// Secp256k1PrivateKeyLength is the [Secp256k1PrivateKey] length in bytes
 const Secp256k1PrivateKeyLength = 32
 
-// Secp256k1PublicKeyLength we use the uncompressed version
+// Secp256k1PublicKeyLength is the [Secp256k1PublicKey] length in bytes.  We use the uncompressed version.
 const Secp256k1PublicKeyLength = 65
 
-// Secp256k1SignatureLength is the Secp256k1 signature without the recovery bit
+// Secp256k1SignatureLength is the [Secp256k1Signature] length in bytes.  It is a signature without the recovery bit.
 const Secp256k1SignatureLength = ethCrypto.SignatureLength - 1
 
-// Secp256k1PrivateKey is a private key that can be used with SingleSigner.  It cannot stand on its own.
-// Implements [MessageSigner], [CryptoMaterial], [bcs.Marshaler], [bcs.Unmarshaler], [bcs.Struct]
+// Secp256k1PrivateKey is a private key that can be used with [SingleSigner].  It cannot stand on its own.
+//
+// Implements:
+//   - [MessageSigner]
+//   - [CryptoMaterial]
+//   - [bcs.Marshaler]
+//   - [bcs.Unmarshaler]
+//   - [bcs.Struct]
 type Secp256k1PrivateKey struct {
-	Inner *ecdsa.PrivateKey
+	Inner *ecdsa.PrivateKey // Inner is the actual private key
 }
 
+// GenerateSecp256k1Key generates a new [Secp256k1PrivateKey]
 func GenerateSecp256k1Key() (*Secp256k1PrivateKey, error) {
 	priv, err := ethCrypto.GenerateKey()
 	if err != nil {
@@ -35,12 +43,20 @@ func GenerateSecp256k1Key() (*Secp256k1PrivateKey, error) {
 
 //region Secp256k1PrivateKey MessageSigner
 
+// VerifyingKey returns the corresponding public key for the private key
+//
+// Implements:
+//   - [MessageSigner]
 func (key *Secp256k1PrivateKey) VerifyingKey() VerifyingKey {
 	return &Secp256k1PublicKey{
 		&key.Inner.PublicKey,
 	}
 }
 
+// SignMessage signs a message and returns the raw [Signature] without a [PublicKey] for verification
+//
+// Implements:
+//   - [MessageSigner]
 func (key *Secp256k1PrivateKey) SignMessage(msg []byte) (sig Signature, err error) {
 	hash := util.Sha3256Hash([][]byte{msg})
 	// TODO: The eth library doesn't protect against malleability issues, so we need to handle those.
@@ -59,10 +75,20 @@ func (key *Secp256k1PrivateKey) SignMessage(msg []byte) (sig Signature, err erro
 
 //region Secp256k1PrivateKey CryptoMaterial
 
+// Bytes outputs the raw byte representation of the [Secp256k1PrivateKey]
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PrivateKey) Bytes() []byte {
 	return ethCrypto.FromECDSA(key.Inner)
 }
 
+// FromBytes populates the [Secp256k1PrivateKey] from bytes
+//
+// Returns an error if the bytes length is not [Secp256k1PrivateKeyLength]
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PrivateKey) FromBytes(bytes []byte) (err error) {
 	if len(bytes) != Secp256k1PrivateKeyLength {
 		return fmt.Errorf("invalid secp256k1 private key size %d", len(bytes))
@@ -71,12 +97,22 @@ func (key *Secp256k1PrivateKey) FromBytes(bytes []byte) (err error) {
 	return err
 }
 
+// ToHex serializes the private key to a hex string
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PrivateKey) ToHex() string {
 	return util.BytesToHex(key.Bytes())
 }
 
 //endregion
 
+// FromHex populates the [Secp256k1PrivateKey] from a hex string
+//
+// Returns an error if the hex string is invalid or is not [Secp256k1PrivateKeyLength] bytes
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PrivateKey) FromHex(hexStr string) (err error) {
 	bytes, err := util.ParseHex(hexStr)
 	if err != nil {
@@ -91,13 +127,26 @@ func (key *Secp256k1PrivateKey) FromHex(hexStr string) (err error) {
 //region Secp256k1PublicKey
 
 // Secp256k1PublicKey is the corresponding public key for [Secp256k1PrivateKey], it cannot be used on its own
-// Implements [VerifyingKey], [PublicKey], [CryptoMaterial], [bcs.Marshaler], [bcs.Unmarshaler], [bcs.Struct]
+//
+// Implements:
+//   - [VerifyingKey]
+//   - [PublicKey]
+//   - [CryptoMaterial]
+//   - [bcs.Marshaler]
+//   - [bcs.Unmarshaler]
+//   - [bcs.Struct]
 type Secp256k1PublicKey struct {
-	Inner *ecdsa.PublicKey
+	Inner *ecdsa.PublicKey // Inner is the actual public key
 }
 
 //region Secp256k1PublicKey VerifyingKey
 
+// Verify verifies the signature of a message
+//
+// Returns true if the signature is valid and a [Secp256k1Signature], false otherwise
+//
+// Implements:
+//   - [VerifyingKey]
 func (key *Secp256k1PublicKey) Verify(msg []byte, sig Signature) bool {
 	switch sig.(type) {
 	case *Secp256k1Signature:
@@ -115,19 +164,35 @@ func (key *Secp256k1PublicKey) Verify(msg []byte, sig Signature) bool {
 
 //region Secp256k1PublicKey CryptoMaterial
 
+// Bytes returns the raw bytes of the [Secp256k1PublicKey]
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PublicKey) Bytes() []byte {
 	return ethCrypto.FromECDSAPub(key.Inner)
 }
 
+// FromBytes sets the [Secp256k1PublicKey] to the given bytes
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PublicKey) FromBytes(bytes []byte) (err error) {
 	key.Inner, err = ethCrypto.UnmarshalPubkey(bytes)
 	return err
 }
 
+// ToHex returns the hex string representation of the [Secp256k1PublicKey], with a leading 0x
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PublicKey) ToHex() string {
 	return util.BytesToHex(key.Bytes())
 }
 
+// FromHex sets the [Secp256k1PublicKey] to the bytes represented by the hex string, with or without a leading 0x
+//
+// Implements:
+//   - [CryptoMaterial]
 func (key *Secp256k1PublicKey) FromHex(hexStr string) (err error) {
 	bytes, err := util.ParseHex(hexStr)
 	if err != nil {
@@ -140,9 +205,18 @@ func (key *Secp256k1PublicKey) FromHex(hexStr string) (err error) {
 
 //region Secp256k1PublicKey bcs.Struct
 
+// MarshalBCS serializes the [Secp256k1PublicKey] to BCS bytes
+//
+// Implements:
+//   - [bcs.Marshaler]
 func (key *Secp256k1PublicKey) MarshalBCS(ser *bcs.Serializer) {
 	ser.WriteBytes(key.Bytes())
 }
+
+// UnmarshalBCS deserializes the [Secp256k1PublicKey] from BCS bytes
+//
+// Implements:
+//   - [bcs.Unmarshaler]
 func (key *Secp256k1PublicKey) UnmarshalBCS(des *bcs.Deserializer) {
 	kb := des.ReadBytes()
 	pubKey, err := ethCrypto.UnmarshalPubkey(kb)
@@ -159,22 +233,39 @@ func (key *Secp256k1PublicKey) UnmarshalBCS(des *bcs.Deserializer) {
 //region Secp256k1Authenticator
 
 // Secp256k1Authenticator is the authenticator for Secp256k1, but it cannot stand on its own and must be used with SingleKeyAuthenticator
-// Implements [AccountAuthenticatorImpl], [bcs.Marshaler], [bcs.Unmarshaler], [bcs.Struct]
+//
+// Implements:
+//   - [AccountAuthenticatorImpl]
+//   - [bcs.Marshaler]
+//   - [bcs.Unmarshaler]
+//   - [bcs.Struct]
 type Secp256k1Authenticator struct {
-	PubKey *Secp256k1PublicKey
-	Sig    *Secp256k1Signature
+	PubKey *Secp256k1PublicKey // PubKey is the public key
+	Sig    *Secp256k1Signature // Sig is the signature
 }
 
 //region Secp256k1Authenticator AccountAuthenticatorImpl
 
+// PublicKey returns the [VerifyingKey] for the authenticator
+//
+// Implements:
+//   - [AccountAuthenticatorImpl]
 func (ea *Secp256k1Authenticator) PublicKey() VerifyingKey {
 	return ea.PubKey
 }
 
+// Signature returns the [Signature] for the authenticator
+//
+// Implements:
+//   - [AccountAuthenticatorImpl]
 func (ea *Secp256k1Authenticator) Signature() Signature {
 	return ea.Sig
 }
 
+// Verify returns true if the authenticator can be cryptographically verified
+//
+// Implements:
+//   - [AccountAuthenticatorImpl]
 func (ea *Secp256k1Authenticator) Verify(msg []byte) bool {
 	return ea.PubKey.Verify(msg, ea.Sig)
 }
@@ -183,11 +274,19 @@ func (ea *Secp256k1Authenticator) Verify(msg []byte) bool {
 
 //region Secp256k1Authenticator bcs.Struct
 
+// MarshalBCS serializes the [Secp256k1Authenticator] to BCS bytes
+//
+// Implements:
+//   - [bcs.Marshaler]
 func (ea *Secp256k1Authenticator) MarshalBCS(ser *bcs.Serializer) {
 	ser.Struct(ea.PublicKey())
 	ser.Struct(ea.Signature())
 }
 
+// UnmarshalBCS deserializes the [Secp256k1Authenticator] from BCS bytes
+//
+// Implements:
+//   - [bcs.Unmarshaler]
 func (ea *Secp256k1Authenticator) UnmarshalBCS(des *bcs.Deserializer) {
 	ea.PubKey = &Secp256k1PublicKey{}
 	des.Struct(ea.PubKey)
@@ -205,17 +304,33 @@ func (ea *Secp256k1Authenticator) UnmarshalBCS(des *bcs.Deserializer) {
 //region Secp256k1Signature
 
 // Secp256k1Signature a wrapper for serialization of Secp256k1 signatures
-// Implements [Signature], [CryptoMaterial], [bcs.Marshaler], [bcs.Unmarshaler], [bcs.Struct]
+//
+// Implements:
+//   - [Signature]
+//   - [CryptoMaterial]
+//   - [bcs.Marshaler]
+//   - [bcs.Unmarshaler]
+//   - [bcs.Struct]
 type Secp256k1Signature struct {
-	Inner []byte
+	Inner []byte // Inner is the actual signature
 }
 
 //region Secp256k1Signature CryptoMaterial
 
+// Bytes returns the raw bytes of the [Secp256k1Signature]
+//
+// Implements:
+//   - [CryptoMaterial]
 func (e *Secp256k1Signature) Bytes() []byte {
 	return e.Inner
 }
 
+// FromBytes sets the [Secp256k1Signature] to the given bytes
+//
+// Returns an error if the bytes length is not [Secp256k1SignatureLength]
+//
+// Implements:
+// - [CryptoMaterial]
 func (e *Secp256k1Signature) FromBytes(bytes []byte) (err error) {
 	if len(bytes) != Secp256k1SignatureLength {
 		return fmt.Errorf("invalid secp256k1 signature size %d, expected %d", len(bytes), Secp256k1SignatureLength)
@@ -224,10 +339,20 @@ func (e *Secp256k1Signature) FromBytes(bytes []byte) (err error) {
 	return nil
 }
 
+// ToHex returns the hex string representation of the [Secp256k1Signature], with a leading 0x
+//
+// Implements:
+//   - [CryptoMaterial]
 func (e *Secp256k1Signature) ToHex() string {
 	return util.BytesToHex(e.Bytes())
 }
 
+// FromHex sets the [Secp256k1Signature] to the bytes represented by the hex string, with or without a leading 0x
+//
+// Returns an error if the hex string is invalid or is not [Secp256k1SignatureLength] bytes
+//
+// Implements:
+//   - [CryptoMaterial]
 func (e *Secp256k1Signature) FromHex(hexStr string) (err error) {
 	bytes, err := util.ParseHex(hexStr)
 	if err != nil {
@@ -240,10 +365,18 @@ func (e *Secp256k1Signature) FromHex(hexStr string) (err error) {
 
 //region Secp256k1Signature bcs.Struct
 
+// MarshalBCS serializes the [Secp256k1Signature] to BCS bytes
+//
+// Implements:
+//   - [bcs.Marshaler]
 func (e *Secp256k1Signature) MarshalBCS(ser *bcs.Serializer) {
 	ser.WriteBytes(e.Bytes())
 }
 
+// UnmarshalBCS deserializes the [Secp256k1Signature] from BCS bytes
+//
+// Implements:
+//   - [bcs.Unmarshaler]
 func (e *Secp256k1Signature) UnmarshalBCS(des *bcs.Deserializer) {
 	bytes := des.ReadBytes()
 	if des.Error() != nil {
