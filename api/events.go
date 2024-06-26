a478bf48809a3977b2ed54764c0e0f49189e1b91
Greg Nazario <greg@gnazar.io>
2024-06-25 22:14:32 -0700

    [events] Add documentation

--- api/events.go
+++ api/events.go
@@ -2,15 +2,50 @@ package api
 
 import "encoding/json"
 
-// Event describes an on-chain event from Move. There are currently two types
-// - V1 events, will have a [GUID] and Sequence number associated
-// - V2 events will not have a [GUID] and Sequence number associated
+//region Event
+
+// Event describes an on-chain event from Move. There are currently two types:
+//
+// - Handle events, will have a [GUID] and Sequence number associated
+//
+//	{
+//		"type": "0x1::coin::WithdrawEvent",
+//		"guid": {
+//			"id": {
+//				"addr": "0x810026ca8291dd88b5b30a1d3ca2edd683d33d06c4a7f7c451d96f6d47bc5e8b",
+//				"creation_num": "3"
+//			}
+//		},
+//		"sequence_number": "0",
+//		"data": {
+//			"amount": "1000"
+//		}
+//	}
+//
+// - Module events will not have a [GUID] and Sequence number associated
+//
+//	{
+//		"type": "0x1::fungible_asset::Withdraw",
+//		"guid": {
+//			"id": {
+//				"addr": "0x0",
+//				"creation_num": "0"
+//			}
+//		},
+//		"sequence_number": "0",
+//		"data": {
+//			"store": "0x1234123412341234123412341234123412341234123412341234123412341234",
+//			"amount": "1000"
+//		}
+//	}
 type Event struct {
-	Type           string // The Move struct type associated with the data
-	Guid           *GUID  // TODO: Remove Guid and Sequence number altogether?
-	SequenceNumber uint64
-	Data           map[string]any // TODO: could be nice to type the events with generated code
+	Type           string         // Type is the fully qualified name of the event e.g. 0x1::coin::WithdrawEvent
+	Guid           *GUID          // GUID is the unique identifier of the event, only present in V1 events
+	SequenceNumber uint64         // SequenceNumber is the sequence number of the event, only present in V1 events
+	Data           map[string]any // Data is the event data, a map of field name to value
 }
+
+//region Event JSON
 
 func (o *Event) MarshalJSON() ([]byte, error) {
 	type inner struct {
@@ -46,3 +81,6 @@ func (o *Event) UnmarshalJSON(b []byte) error {
 	o.Data = data.Data
 	return nil
 }
+
+//endregion
+//endregion
