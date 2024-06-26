46d358fa5b33516285de063a385a3689b8f3ce74
Greg Nazario <greg@gnazar.io>
2024-06-26 11:55:48 -0700

    [API] add documentation to all types

--- api/module_test.go
+++ api/module_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestModule_MoveBytecode tests the MoveBytecode struct
 func TestModule_MoveBytecode(t *testing.T) {
 	testJson := `{
 		"bytecode": "0xa11ceb0b060000000901000202020403060f0515"
@@ -19,6 +20,7 @@ func TestModule_MoveBytecode(t *testing.T) {
 	assert.Equal(t, HexBytes(expectedRes), data.Bytecode)
 }
 
+// TestModule_MoveScript tests the MoveScript struct
 func TestModule_MoveScript(t *testing.T) {
 	testJson := `{
 		"bytecode": "0xa11ceb0b060000000901000202020403060f0515"
