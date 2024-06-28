a3a0819a5be91fac5d675bed3fc5f569e38c1f70
Greg Nazario <greg@gnazar.io>
2024-06-27 21:42:14 -0700

    [doc] Address comments from previous PR

---
+++ api/doc.go
@@ -0,0 +1,9 @@
+// Package api represents all types associated with the Aptos REST API.  It handles JSON packing and un-packing, through
+// multiple inner types.
+//
+// Quick links:
+//
+//   - [Aptos API Reference] for an interactive OpenAPI documentation experience.
+//
+// [Aptos API Reference]: https://aptos.dev/en/build/apis/fullnode-rest-api-reference
+package api
