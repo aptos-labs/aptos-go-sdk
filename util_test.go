5b65caca3a064b0df4611d5660cecbc2ba42c604
Greg Nazario <greg@gnazar.io>
2024-07-24 02:57:45 -0700

    [util] Add util test coverage

--- util_test.go
+++ util_test.go
@@ -2,6 +2,7 @@ package aptos
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/big"
 	"testing"
 )
 
@@ -29,5 +30,46 @@ func TestParseHex(t *testing.T) {
 		val, err := ParseHex(input)
 		assert.NoError(t, err)
 		assert.Equal(t, expected[i], val)
+	}
+}
+
+func TestBytesToHex(t *testing.T) {
+	inputs := [][]byte{{0x01, 0x23, 0x45}, {0x01}, {}}
+	expected := []string{"0x012345", "0x01", "0x"}
+
+	for i, input := range inputs {
+		val := BytesToHex(input)
+		assert.Equal(t, expected[i], val)
+	}
+}
+
+func TestStrToUint64(t *testing.T) {
+	inputs := []string{"0", "1", "100"}
+	expected := []uint64{0, 1, 100}
+
+	for i, input := range inputs {
+		val, err := StrToUint64(input)
+		assert.NoError(t, err)
+		assert.Equal(t, expected[i], val)
+	}
+}
+
+func TestStrToBigInt(t *testing.T) {
+	inputs := []string{"0", "1", "100"}
+	expected := []*big.Int{big.NewInt(0), big.NewInt(1), big.NewInt(100)}
+
+	for i, input := range inputs {
+		val, err := StrToBigInt(input)
+		assert.NoError(t, err)
+		assert.Equal(t, expected[i], val)
+	}
+}
+
+func TestStrToBigIntError(t *testing.T) {
+	inputs := []string{"hello", "1a", "", "0.01"}
+
+	for _, input := range inputs {
+		_, err := StrToBigInt(input)
+		assert.Error(t, err)
 	}
 }
